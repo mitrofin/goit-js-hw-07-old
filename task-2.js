8b0e9e2e7c3b8075638985d0dde37e364f792b83
mitrofin <mitrofn@gmail.com>
2020-10-03 23:46:30 +0300

    Update task-2.js

--- task-2.js
+++ task-2.js
@@ -7,6 +7,11 @@ const ingredients = [
   'Приправы',
 ];
 
-const listRef = document.getElementById('ingredients');
-
 console.log(ingredients);
+
+const listOfItem = ingredients.map(ingredient => {
+  const item = document.createElement('li');
+  item.textContent = ingredient;
+  return item;
+});
+const listRef = document.getElementById('ingredients').append(...listOfItem);
